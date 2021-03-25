84881890548eb0d41bc153e22eea63306d312623
Philippe Leroux <philz1213@hotmail.com>
2021-03-25 13:34:31 -0400

    First push

---
+++ dist/BackEnd/app.js
@@ -0,0 +1,22 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+mongoose.connect('mongodb+srv://admin:XuMAsiPDJy1BjJhS@cluster0.ydqp1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+app.use(bodyParser.json());
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/FrontEnd/index.html'));
+});
+module.exports = app;
